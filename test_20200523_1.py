56051c514ed7c2971792ba6317167aca2904018f
LemonGuai <csf2412297817@163.com>
2020-05-23 15:19:26 +0800

    push

---
+++ test_20200523_1.py
@@ -0,0 +1,26 @@
+# 编写一个函数来查找字符串数组中的最长公共前缀。
+
+# 如果不存在公共前缀，返回空字符串 ""。
+
+# 示例 1:
+
+# 输入: ["flower","flow","flight"]
+# 输出: "fl"
+# 示例 2:
+
+# 输入: ["dog","racecar","car"]
+# 输出: ""
+# 解释: 输入不存在公共前缀。
+# 说明:
+
+# 所有输入只包含小写字母 a-z 。
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strlen = len(strs)
+        string = ""
+        for i in zip(*strs):
+            if len(set(i)) == 1:
+                string += i[0]
+            else:
+                break
+        return string
